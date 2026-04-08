7edbd3ddfc1e5f92c401ee19ce092c14e6522652
Esteban Ortega <1151272+eortegaz@users.noreply.github.com>
2026-04-08 14:17:31 -0400

    Update download script to use new APK URL and user agent

--- downloadassets.sh
+++ downloadassets.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-curl "http://121.40.123.198:8080/AutoKit/AutoKit.apk" > AutoKit.apk
+curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" \
+     -e "https://www.carlinkit.com/download.html" \
+     -L "https://omo-oss-file110.thefastfile.com/portal-saas/pg2026012818440705289/cms/file/autokit_2025.03.19.1126.apk" \
+     -o AutoKit.apk
 unzip AutoKit.apk 'assets/*'
-
